1ef59cf03d34d780d167c0433b904efcfe022825
Van <549804563@qq.com>
2018-12-20 13:34:57 +0800

    fix -- 修改请求域名

--- src/components/Global.vue
+++ src/components/Global.vue
@@ -1,6 +1,6 @@
 <script>
 // const hostUrl = '/admin'
-const hostUrl = 'https://qun.sync163.com/admin/'
+const hostUrl = 'https://qun.sync163.com/admin'
 // const imgHostUrl = ''
 export default {
   hostUrl
